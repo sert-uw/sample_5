1a75d7f9d564319273304449697bccbe5a2bc2f8
sert <demo.sert@gmail.com>
2016-06-12 18:54:09 +0900

    run 'rake db:migrate'

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160612065614) do
+ActiveRecord::Schema.define(version: 20160612095328) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
     t.string   "maker"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "softwares", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "price"
+    t.string   "maker"
+    t.string   "hard"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
